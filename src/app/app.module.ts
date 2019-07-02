d214a13f1e029a6035e23122231fc93e7ab11371
yanomami <yanomami92@gmail.com>
2019-07-02 11:06:21 +0200

    misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCountryComponent } from './country/list-country/list-country.component';
+import { AddCountryComponent } from './country/add-country/add-country.component';
+import { EditCountryComponent } from './country/edit-country/edit-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCountryComponent,
+    AddCountryComponent,
+    EditCountryComponent
   ],
   imports: [
     BrowserModule,
