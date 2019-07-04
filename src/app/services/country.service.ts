60ad7468f0eb3e327ff28f2282076d44ac681cf3
yanomami <yanomami92@gmail.com>
2019-07-04 14:30:13 +0200

    misc

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -8,21 +8,22 @@ import {ListResult} from '../models/list-result.model';
 })
 export class CountryService {
 
-  private url = 'http://localhost:8080/alexandro/countries';
+  private apiUrl = 'http://localhost:8080/alexandro/';
+  private endPoint = this. apiUrl + 'countries';
 
   constructor(private http: HttpClient) {
   }
 
   public getList(page: number, size: number) {
     return this.http.get<ListResult<Country>>(
-      this.url
+      this.endPoint
       + '?page=' + page
       + '&size=' + size);
   }
 
   public getListByDescription(keyword: string, page: number, size: number) {
     return this.http.get<ListResult<Country>>(
-      this.url
+      this.endPoint
       + '/search/byDescription'
       + '?key=' + keyword
       + '&page=' + page
@@ -33,24 +34,24 @@ export class CountryService {
   // You just need to parse your JSON accordingly to the HAL specification used by Spring Data Rest.
   // ->   public getSingle(url: string)
   public getSingle(id: number) {
-    return this.http.get<Country>(this.url + '/' + id);
+    return this.http.get<Country>(this.endPoint + '/' + id);
   }
 
   // The best solution would be not to using the IDs of your entities, and use the link references the hypermedia provides.
   // You just need to parse your JSON accordingly to the HAL specification used by Spring Data Rest.
   // ->   public delete(url: string)
   public delete(resource: Country) {
-    return this.http.delete(this.url + '/' + resource.idCountry);
+    return this.http.delete(this.endPoint + '/' + resource.idCountry);
   }
 
   public create(resource: Country) {
-    return this.http.post(this.url, resource);
+    return this.http.post(this.endPoint, resource);
   }
 
   // The best solution would be not to using the IDs of your entities, and use the link references the hypermedia provides.
   // You just need to parse your JSON accordingly to the HAL specification used by Spring Data Rest.
   // ->   public update(url: string, resource: Foo)
   public update(resource: Country) {
-    return this.http.put(this.url + '/' + resource.idCountry, resource);
+    return this.http.put(this.endPoint + '/' + resource.idCountry, resource);
   }
 }
