71e1868175663e26e8a71195f10c4f4c32a01cc9
yanomami <yanomami92@gmail.com>
2019-08-08 21:22:52 +0200

    misc

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CaddyService} from './caddy.service';
+import {Caddy} from '../models/caddy.model';
+import {OrderHeader} from '../models/orderHeader.model';
+import {Client} from '../models/client.model';
+import {ShippingMethod} from '../models/shippingMethod.model';
+import {OrderLine} from '../models/orderLine.model';
+import {Product} from '../models/product.model';
+import {ProductItem} from '../models/product-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  private host  = 'http://localhost:8080/alexandro';
+
+  constructor(private http: HttpClient,
+              private caddyService: CaddyService ) { }
+
+  submitOrder() {
+    // return this.http.post(this.host + '/orders', this.order);
+  }
+
+  getOrder(id: number): Observable<OrderHeader> {
+    return this.http.get<OrderHeader>(this.host + 'orders' + id);
+  }
+
+  makeOrderFromCaddy() {}
 }
