4f675d2bac34e5cfa0d7bd899a41c7f4420ff60f
yanomami <yanomami92@gmail.com>
2019-08-08 18:54:32 +0200

    misc

---
+++ src/app/models/orderLinemodel.ts
@@ -0,0 +1,10 @@
+import {OrderHeader} from './orderHeader.model';
+import {Product} from './product.model';
+
+export class OrderLine {
+  orderHeaderId: number;
+  productId: number;
+  quantity: number;
+  orderHeaderByOrderHeaderId: OrderHeader;
+  productByProductId: Product;
+}
