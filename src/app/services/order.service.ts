78d6d49b6545f41984caff36908137bfe47d29e7
yanomami <yanomami92@gmail.com>
2019-08-09 12:11:21 +0200

    misc

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {CaddyService} from './caddy.service';
 import {OrderHeader} from '../models/orderHeader.model';
-import {Order} from '../models/order.model';
+import {Item, Order} from '../models/order.model';
 import {Caddy} from '../models/caddy.model';
 import {ProductItem} from '../models/product-item.model';
 
@@ -18,9 +18,9 @@ export class OrderService {
   constructor(private http: HttpClient,
               private caddyService: CaddyService ) { }
 
-  submitOrder() {
+  submitOrder(): Observable<Order> {
     this.makeOrderFromCaddy();
-    return this.http.post(this.host + '/orders', this.order);
+    return this.http.post<Order>(this.host + '/orders', this.order);
   }
 
   getOrder(id: number): Observable<OrderHeader> {
@@ -30,10 +30,20 @@ export class OrderService {
   private makeOrderFromCaddy() {
     const caddy: Caddy = this.caddyService.getCurrentCaddy();
     this.order.client = caddy.client;
+
     const productItems: ProductItem[] = this.caddyService.getCurrentCaddyItemsArray();
-    for ( let i = 0; i < productItems.length; i++) {
+
+    productItems.forEach( p => {
+      const item = new Item();
+      item.id = p.product.id;
+      item.quantity = p.quantity;
+      this.order.productItems.push(item);
+    });
+
+/*    for ( let i = 0; i < productItems.length; i++) {
+      this.order.productItems[i] = new Item();
       this.order.productItems[i].id = productItems[i].product.id;
       this.order.productItems[i].quantity = productItems[i].quantity;
-    }
+    }*/
   }
 }
